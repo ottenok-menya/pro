eee488af0086a84c7ac84d182f32f4d13f68e128
ottenok-menya <vbakq1@gmail.com>
2021-05-04 21:01:44 +0600

    new root

--- pro_backend/pro/settings.py
+++ pro_backend/pro/settings.py
@@ -127,7 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
